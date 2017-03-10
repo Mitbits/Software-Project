5df21704f92c336a2370291aa43686e287d71d6c
prabh961 <prabh961@gmail.com>
2017-03-10 11:12:33 -0500

    Added login page and floor plan folder

---
+++ app/lib/routes.js
@@ -0,0 +1,15 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+    action: function() {
+        BlazeLayout.render("landingpage");
+    }
+});
+FlowRouter.route('/floorplan', {
+    action: function() {
+        BlazeLayout.render("floorplan");
+    }
+});
+
+
