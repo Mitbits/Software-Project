cc1991658e654ce1e57a923b554ebcb2c1fa9973
prabh961 <prabh961@gmail.com>
2017-03-15 22:59:38 -0400

    more updates to floorplan ui

--- app/client/floorPlan/floorplan.js
+++ app/client/floorPlan/floorplan.js
@@ -4,22 +4,53 @@ import { Tables,Table, TableStatus } from '../../imports/api/table.js';
 Template.table.events({
     'click .icon.link' () {
         console.log(event.currentTarget.id);
-        $('.ui.modal')
-            .modal('show')
-        ;
-
     },
     'click .set_taken' () {
     	Tables.update(this._id,{
-		$set: { table_status : TableStatus.TAKEN},
+		$set: { table_status : TableStatus.RESERVED},
 	});
-    
-    }
+    },
+    'click .link.card' () {
+        console.log(this._id);
+        $("#this._id")
+            .modal('show')
+        ;
+    },
+    'click .red.right.corner.label' ()
+    {
+        Tables.update(this._id,{
+            $set: { table_status : TableStatus.CLEAN}
+        });
+    },
 });
 
 Template.floorplan.helpers({
     tables()
     {
         return Table.find({});
+    },
+
+});
+
+Template.table.helpers({
+    'isDirty': function() {
+        if(this.table_status == 'Dirty') {
+            return true;
+        }
+    },
+    'isClean': function() {
+        if(this.table_status == 'Clean') {
+            return true;
+        }
+    },
+    'isReserved': function() {
+        if(this.table_status == 'Reserved') {
+            return true;
+        }
+    },
+    'isTaken': function() {
+        if(this.table_status == 'Taken') {
+            return true;
+        }
     },
 });
