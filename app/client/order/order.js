adfa56f4193e5e2299a1dc9ef7d5d4ebdaf955df
Apacrathon <apacrathon@gmail.com>
2017-03-22 16:40:56 -0400

    Created helper function to retrieve order table

--- app/client/order/order.js
+++ app/client/order/order.js
@@ -1,16 +1,18 @@
-/**
- * Created by mitpatel on 3/16/17.
- */
- Template.orderQueue.helpers({
-   templateGestures: {
-     'doubletap div div#hammertap': function(event, templateInstance) {
-       var $deleteObj = $(event.target.parentNode);
-       $deleteObj.fadeOut("slow", function() {
-         $(this).remove();
-       });
+import '../../imports/api/orders.js';
+import { Orders } from '../../imports/api/orders.js';
 
-     }
-   }
-
- });
+Template.orderQueue.helpers({
+	templateGestures: {
+		'doubletap div div#hammertap': function(event, templateInstance) {
+			var $deleteObj = $(event.target.parentNode);
+			$deleteObj.fadeOut("slow", function() {
+				$(this).remove();
+			});
+		}
+	},
+	'orders': function() {
+		//console.log(Orders.find());
+		return Orders.find({});
+	},
+});
 
