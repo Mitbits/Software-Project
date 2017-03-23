1f0ac77e9982c8e448dddd8432d34e02090d5b23
Moulindra Muchumari <moulindra1729@gmail.com>
2017-03-23 17:14:59 -0400

    Added timers and changed double tap to button, added time add feature

--- app/client/order/order.js
+++ app/client/order/order.js
@@ -1,13 +1,92 @@
+import {CountDownTimer} from './CountDownTimer.js'
 /**
  * Created by mitpatel on 3/16/17.
  */
  Template.orderQueue.helpers({
    templateGestures: {
-     'doubletap div div#hammertap': function(event, templateInstance) {
-       var $deleteObj = $(event.target.parentNode);
-       $deleteObj.fadeOut("slow", function() {
-         $(this).remove();
-       });
+     'tap div div#hammertap': function(event, templateInstance) {
+       // check if user clicked on the buttons
+       console.log(event.target.id);
+       var id = event.target.id;
+       if(id == 'done-btn' || id == 'done-btn-icon') {
+         addButtonHandler(event, templateInstance);
+         return;
+       } else if(id == 'add-time-btn' || id == 'add-time-icon') {
+         addTimeHandler(event, templateInstance);
+         return;
+       }
+       var $colorObj = $(event.target.parentNode);
+       var classesApplied = $colorObj.attr('class');
+       var isActive = classesApplied.includes('active');
+       var $addTimeBtn = $colorObj.find('#add-time-btn');
+       if(isActive) { //change it to normal- white
+         $colorObj.removeClass('active');
+         $colorObj.addClass('white');
+         $addTimeBtn.addClass('disabled');
+         resetTimer(event);
+       } else {
+         $colorObj.removeClass('white');
+         $colorObj.addClass('active');
+         $addTimeBtn.removeClass('disabled');
+         startTimer(event);
+       }
      }
    }
  });
+
+var timer;
+
+var addButtonHandler = function(event, templateInstance) {
+  var $selectedObj = $(event.target);
+  var $deleteObj = $selectedObj.closest('#hammertap');
+  $deleteObj.fadeOut('slow', function() {
+    $(this).remove();
+  });
+}
+
+var deltaTime = 30;
+var addTimeHandler = function(event, templateInstance) {
+  var {timeMin, timeSec, timeText, $timeObj} = getTime(event);
+  var duration = 60*timeMin + timeSec + deltaTime;
+  resetTimer(event);
+  startCountDown($timeObj, duration, timeText);
+}
+
+var startTimer = function(event) {
+  var {timeMin, timeSec, timeText, $timeObj} = getTime(event);
+  var duration = 60*timeMin + timeSec;
+  console.log(typeof timeMin);
+  console.log(duration);
+  $timeObj.text(1 + ":" + 23);
+  startCountDown($timeObj, duration, timeText);
+}
+
+var getTime = function(event) {
+  var $selectedObj = $(event.target);
+  var $timeObj = $selectedObj.closest('#hammertap').find('#time');
+  var timeText = $timeObj.text();
+  var timeMin = 1*timeText.substring(0, timeText.indexOf(':'));
+  var timeSec = 1*timeText.substring(timeText.indexOf(':')+1);
+  console.log(timeMin);
+  console.log(timeSec);
+  return {timeMin, timeSec, timeText, $timeObj};
+}
+
+var startCountDown = function($timeObj, duration, resetTimeText) {
+  timer = new CountDownTimer(duration, 1000);
+  //alert(duration);
+  console.log($timeObj);
+  timer.onTick(function(min, sec, reset) {
+    $timeObj.text(min + ":" + sec);
+    if(reset) { // reset timer
+      $timeObj.text(resetTimeText);
+    }
+  });
+  timer.start();
+}
+
+var resetTimer = function(event) {
+  var $selectedObj = $(event.target);
+  var $timeObj = $selectedObj.closest('#hammertap').find('#time');
+  timer.resetTimer();
+}
