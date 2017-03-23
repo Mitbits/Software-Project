c6e82dae64179297d56306a6ed0adb2a4105424e
Mit Patel <mitpatel@Mits-MacBook-Pro.local>
2017-03-23 17:39:00 -0400

    added different tab pages

--- app/client/waiter/waiter.js
+++ app/client/waiter/waiter.js
@@ -4,11 +4,29 @@
 Template.waiter.events({
     'click .Appetizers' () {
         document.getElementById("entreeMenu").className = "displayNone";
+        document.getElementById("floorPlan").className = "displayNone";
         document.getElementById("appMenu").className = "displayAll";
+        document.getElementById("dessertsMenu").className = "displayNone";
     },
     'click .Entrees' () {
         document.getElementById("appMenu").className = "displayNone";
+        document.getElementById("floorPlan").className = "displayNone";
         document.getElementById("entreeMenu").className = "displayAll";
-
+        document.getElementById("dessertsMenu").className = "displayNone";
     },
+
+    'click .FloorPlan' () {
+        document.getElementById("floorPlan").className = "displayAll";
+        document.getElementById("entreeMenu").className = "displayNone";
+        document.getElementById("appMenu").className = "displayNone";
+        document.getElementById("dessertsMenu").className = "displayNone";
+    },
+
+    'click .Desserts' () {
+        document.getElementById("floorPlan").className = "displayNone";
+        document.getElementById("entreeMenu").className = "displayNone";
+        document.getElementById("appMenu").className = "displayNone";
+        document.getElementById("dessertsMenu").className = "displayAll";
+
+    }
 });
