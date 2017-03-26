5a4b26249113caa6ad87eb63aab800d6027f561e
Moulindra Muchumari <moulindra1729@gmail.com>
2017-03-26 13:12:31 -0400

    changed timer functions to run multiple timers, added red color to div when it expires and added undo button for done button

--- app/client/order/order.js
+++ app/client/order/order.js
@@ -4,52 +4,72 @@ import { Order, Orders } from '../../imports/api/order.js';
 import '../../imports/api/priorityManager.js';
 import { PriorityManager } from '../../imports/api/priorityManager.js';
 
-Template.orderQueue.helpers({
-	templateGestures: {
-		'tap div div#hammertap': function(event, templateInstance) {
-			// check if user clicked on the buttons
-			console.log(event.target.id);
-			var id = event.target.id;
-			if(id == 'done-btn' || id == 'done-btn-icon') {
-				addButtonHandler(event, templateInstance);
-				return;
-			} else if(id == 'add-time-btn' || id == 'add-time-icon') {
-				addTimeHandler(event, templateInstance);
-				return;
-			}
-
-			var $colorObj = $(event.target.parentNode);
-			var classesApplied = $colorObj.attr('class');
-			var isActive = classesApplied.includes('active');
-			var $addTimeBtn = $colorObj.find('#add-time-btn');
-			
-			if(isActive) { //change it to normal- white
-				$colorObj.removeClass('active');
-				$colorObj.addClass('white');
-				$addTimeBtn.addClass('disabled');
-				resetTimer(event);
-			} else {
-				$colorObj.removeClass('white');
-				$colorObj.addClass('active');
-				$addTimeBtn.removeClass('disabled');
-				startTimer(event);
-			}
+Template.orderRow.events({
+	'click div#tap': function(event, templateInstance) {
+		// check if user clicked on the buttons
+		if (!templateInstance.data.timer) {
+			console.log('timer not found');
 		}
-	},
-	orders() {
-		console.log(PriorityManager.start());
-		return PriorityManager.start();
+		var id = event.target.id;
+		if(id == 'done-btn' || id == 'done-btn-icon') {
+			doneButtonHandler(event, templateInstance);
+			return;
+		} else if(id == 'add-time-btn' || id == 'add-time-icon') {
+			addTimeHandler(event, templateInstance);
+			return;
+		}
 
+		var $colorObj = $(event.target.parentNode);
+		var classesApplied = $colorObj.attr('class');
+		var isActive = classesApplied.includes('active');
+		var $addTimeBtn = $colorObj.find('#add-time-btn');
+
+		if(isActive) { //change it to normal- white
+			$colorObj.removeClass('active');
+			$colorObj.addClass('white');
+			$addTimeBtn.addClass('disabled');
+			resetTimer(event, templateInstance);
+		} else {
+			$colorObj.removeClass('white');
+			$colorObj.addClass('active');
+			$addTimeBtn.removeClass('disabled');
+			startTimer(event, templateInstance);
+		}
 	}
 });
 
-var timer;
+Template.orderRow.onRendered(function() {
+	// create timer
+	var {timeMin, timeSec, timeText, $timeObj} = getTime(null, this.$('div#time'));
+	var duration = 60*timeMin + timeSec;
+	this.data.timer = new CountDownTimer(duration, 1000);
+	this.$('div#undo').hide();
+});
 
-var addButtonHandler = function(event, templateInstance) {
+Template.orderQueue.helpers({
+	orders() {
+		return PriorityManager.start();
+	}
+});
+
+var doneButtonHandler = function(event, templateInstance) {
 	var $selectedObj = $(event.target);
-	var $deleteObj = $selectedObj.closest('#hammertap');
+	var $deleteObj = $selectedObj.closest('#tap');
 	$deleteObj.fadeOut('slow', function() {
-		$(this).remove();
+		var $undoObj = $deleteObj.next();
+		$undoObj.show();
+		var clicked = false;
+		$undoObj.find('#undo-btn').click(function() {
+			$undoObj.hide();
+			$deleteObj.show();
+			clicked = true;
+		});
+		setTimeout(function() {
+			if(!clicked) {
+				$deleteObj.remove();
+				$undoObj.remove();
+			}
+		}, 10000);
 	});
 }
 
@@ -57,45 +77,79 @@ var deltaTime = 30;
 var addTimeHandler = function(event, templateInstance) {
 	var { timeMin, timeSec, timeText, $timeObj } = getTime(event);
 	var duration = 60*timeMin + timeSec + deltaTime;
-	resetTimer(event);
-	startCountDown($timeObj, duration, timeText);
+	var time = CountDownTimer.parse(duration);
+	timeText = timeToString(time['minutes'], time['seconds']);
+	resetTimer(event, templateInstance);
+	if(templateInstance.data.timer == null) {
+		templateInstance.data.timer = new CountDownTimer(duration, 1000);
+	}
+	startCountDown($timeObj, duration, timeText, templateInstance.data.timer);
 }
 
-var startTimer = function(event) {
+var startTimer = function(event, templateInstance) {
 	var {timeMin, timeSec, timeText, $timeObj} = getTime(event);
 	var duration = 60*timeMin + timeSec;
-	console.log(typeof timeMin);
-	console.log(duration);
-	$timeObj.text(1 + ":" + 23);
-	startCountDown($timeObj, duration, timeText);
+	if(templateInstance.data.timer == null) {
+		templateInstance.data.timer = new CountDownTimer(duration, 1000);
+	}
+	startCountDown($timeObj, duration, timeText, templateInstance.data.timer);
 }
 
-var getTime = function(event) {
-	var $selectedObj = $(event.target);
-	var $timeObj = $selectedObj.closest('#hammertap').find('#time');
+var getTime = function(event, $timeObj) {
+	if(!$timeObj) {
+		var $selectedObj = $(event.target);
+		$timeObj = $selectedObj.closest('#tap').find('#time');
+	}
 	var timeText = $timeObj.text();
 	var timeMin = 1*timeText.substring(0, timeText.indexOf(':'));
 	var timeSec = 1*timeText.substring(timeText.indexOf(':')+1);
-  console.log(timeMin);
-  console.log(timeSec);
   return {timeMin, timeSec, timeText, $timeObj};
 }
 
-var startCountDown = function($timeObj, duration, resetTimeText) {
-	timer = new CountDownTimer(duration, 1000);
-	//alert(duration);
-	console.log($timeObj);
+var timeToString = function(min, sec) {
+	var timeText = '';
+	if(min < 10) {
+		timeText += '0'+min;
+	} else {
+		timeText += min;
+	}
+
+	if(sec < 10) {
+		timeText += ':0'+sec;
+	} else {
+		timeText += ":"+sec;
+	}
+	return timeText;
+}
+
+var startCountDown = function($timeObj, duration, resetTimeText, timer) {
+	//var timer = new CountDownTimer(duration, 1000);
 	timer.onTick(function(min, sec, reset) {
-		$timeObj.text(min + ":" + sec);
+		if(timer.expired()) {
+			timerExpired($timeObj);
+		}
 		if(reset) { // reset timer
 			$timeObj.text(resetTimeText);
+		} else {
+			$timeObj.text(timeToString(min, sec));
 		}
 	});
   timer.start();
 }
 
-var resetTimer = function(event) {
-	var $selectedObj = $(event.target);
-	var $timeObj = $selectedObj.closest('#hammertap').find('#time');
-	timer.resetTimer();
+var resetTimer = function(event, templateInstance) {
+	/*if(!timeText) {
+		var $selectedObj = $(event.target);
+	 	timeText = $selectedObj.closest('#tap').find('#time').text();
+	}*/
+
+	templateInstance.data.timer.resetTimer();
+	templateInstance.data.timer = null;
+}
+
+var timerExpired = function($timeObj) {
+	var $orderRow = $timeObj.closest('div#tap');
+	$orderRow.removeClass('white');
+	$orderRow.removeClass('active');
+	$orderRow.addClass('expired');
 }
