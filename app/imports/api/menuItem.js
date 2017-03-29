620af7629e543a23c1470ec36d74179fc3a7dd39
Apacrathon <apacrathon@gmail.com>
2017-03-28 21:25:18 -0400

    Updated with JSDoc-style comments

--- app/imports/api/menuItem.js
+++ app/imports/api/menuItem.js
@@ -3,23 +3,36 @@ import {Class, Enum} from 'meteor/jagi:astronomy';
 
 export const MenuItems = new Mongo.Collection('menuitems');
 
-/*
- * ENUM type definitions
+/**
+ * @readonly
+ * @enum {number}
+ * Enum for order types.
  */
-
-// Definition for Order types.
 export const ORDER_TYPE = Enum.create({
 	name: 'orderType',
 	identifiers: {
+		/** Basic meal stages*/
 		REFRESHMENT: 0,
 		APPETIZER: 1,
 		ENTREE: 2,
 		DESSERT: 3,
+		/** Unclassified orders*/
 		OTHER: 4,
+		/** Cancelled order*/
 		CANCELLED: 5
 	}
 });
 
+/**
+ * @class
+ * @classdesc Class representing an item on a menu.
+ * @param {Number} itemID - Unique item identifier
+ * @param {String} itemName - Name of the item
+ * @param {String} itemDescription - Describes the item
+ * @param {ORDER_TYPE} mealType - The type of item
+ * @param {Number} itemPrice - The cost of the item
+ * @param {Number} cookTime - Time to make the item in minutes
+ */
 export const MenuItem = Class.create({
     name: 'MenuItem',
 	collection: MenuItems,
--- app/imports/api/order.js
+++ app/imports/api/order.js
@@ -6,24 +6,33 @@ import { MenuItem } from './menuItem.js';
 export const Orders = new Mongo.Collection('orders');
 export const Customers = new Mongo.Collection('customers');
 
+/**
+ * @global
+ * @desc Validation parameter for unique ID numbers used in various classes.
+ * @type {[*]}
+ */
 var minID = [{
 	type: 'gt',
 	param: 0
 }]
 
-/*
- * Custom types
+/**
+ * A single item belonging to a set of one or more orderItems of a single order
+ * @typedef {orderItem}
  */
-
 Type.create({
 	name: 'orderItem',
 	class: 'orderItem'
 })
 
-/*
- * Class definitions
+/**
+ * @class
+ * @classdesc - Represents an item for an order.
+ * @param {Number} itemID - Unique identifier representing an item within an order
+ * @param {Number} priority - Priority number for item in the order queue
+ * @param {Number} menuItemID - Indicates the menuItem this item represents
+ * @param {String} specialRequests - Contains any requests by the customer to notify the chef about a particular item
  */
-
 export const orderItem = Class.create({
 	name: 'orderItem',
 	fields: {
@@ -43,6 +52,17 @@ export const orderItem = Class.create({
 	}
 });
 
+/**
+ * @class
+ * @classdesc Represents an Order
+ * @param {Number} orderID - Unique order identifier
+ * @param {Number} waiterID - Waiter identifier for waiter that placed the order
+ * @param {Array<orderItem>} - Contains all items for order
+ * @param {Date} timePlaced - Time and date the order was created
+ *
+ * @todo Extend the order class to include a table identifier (integrate with the table class).
+ * @todo Add meteor method(s) to extend the functionality of placing and viewing live orders.
+ */
 export const Order = Class.create({
 	name: 'Order',
 	collection: Orders,
@@ -73,7 +93,10 @@ export const Order = Class.create({
 		}
 	},
 	meteorMethods: {
-		// Elementary function required for Demo 1 functionality.
+		/**
+		 * @func Makes an order available to the order queue by storing an 'order' document in the database
+		 * @this Refers to an `Order` object handled by a waiter
+		 */
 		placeOrder() {
 			this.save();
 		}
