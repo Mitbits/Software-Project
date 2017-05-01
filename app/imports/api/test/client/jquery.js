9e117205e55159a85c33c2780f949f6342750312
Moulindra Muchumari <moulindra1729@gmail.com>
2017-04-30 22:49:34 -0400

    inventory tests added

--- app/imports/api/test/client/jquery.js
+++ app/imports/api/test/client/jquery.js
@@ -17,6 +17,19 @@ export const jQuery = function(arg){
         this.containsGraphs = function(num) {
             console.log('checking graphs...');
         }
+
+        this.clickBuyBtn = function(item) {
+            console.log('buying item...');
+        }
+
+        this.checkShoppingList = function(item) {
+            console.log('checking shopping list...');
+        }
+
+        this.clickRmBtn = function(item) {
+            console.log('removing item...');
+        }
+
 	return this;
 
 }
